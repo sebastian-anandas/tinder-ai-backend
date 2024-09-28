b4be63e9648e46c77e3d9a2604024cd0bb8ed373
sebastian-anandas <sebastian.anandas@gmail.com>
2024-09-28 13:24:30 +0530

    Basic Initializer app in working state

---
+++ src/test/java/io/seb/tinderaibackend/TinderAiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.seb.tinderaibackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TinderAiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
